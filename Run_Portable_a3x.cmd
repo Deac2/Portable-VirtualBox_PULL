4f7391eaa252d1534f5de4e95e2dbf7ba4d1f40b
Deac2 <159959206+Deac2@users.noreply.github.com>
2024-10-30 20:36:34 +0300

    Add files via upload

--- Run_Portable_a3x.cmd
+++ Run_Portable_a3x.cmd
@@ -1,9 +1,9 @@
 @echo off
 @chcp 65001>Nul
 
-for /f "tokens=*" %%i in ('dir /b /s *Box*.a3x') do set a3x=%%i
+for /f "tokens=*" %%i in ('dir /b/s *Box*.a3x') do set a3x=%%i
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
-start /b AutoIt3.exe "%a3x%"
+start /b "" %CD%/AutoIt3.exe "%a3x%"
 ) else (
-start /b AutoIt3_x64.exe "%a3x%"
+start /b "" %CD%/AutoIt3_x64.exe "%a3x%"
 )
