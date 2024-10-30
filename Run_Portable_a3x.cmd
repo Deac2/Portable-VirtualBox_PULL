53dc4ab61e09ab90084b5aec224ff678179779fc
Deac2 <159959206+Deac2@users.noreply.github.com>
2024-10-30 22:12:56 +0300

    Update Run_Portable_a3x.cmd

--- Run_Portable_a3x.cmd
+++ Run_Portable_a3x.cmd
@@ -3,7 +3,7 @@
 
 for /f "tokens=*" %%i in ('dir /b/s *Box*.a3x') do set a3x=%%i
 if "%PROCESSOR_ARCHITECTURE%"=="x86" (
-start /b "" %CD%/AutoIt3.exe "%a3x%"
+start /b "" "%CD%"/AutoIt3.exe "%a3x%"
 ) else (
-start /b "" %CD%/AutoIt3_x64.exe "%a3x%"
+start /b "" "%CD%"/AutoIt3_x64.exe "%a3x%"
 )
