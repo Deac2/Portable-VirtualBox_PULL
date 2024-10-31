09dd7fee9ca5ae3db9971db54212d6d33f50d136
Deac2 <159959206+Deac2@users.noreply.github.com>
2024-10-31 12:22:15 +0300

    Delete Run_Portable_a3x.cmd

--- Run_Portable_a3x.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-@chcp 65001>Nul
-
-for /f "tokens=*" %%i in ('dir /b/s *Box*.a3x') do set a3x=%%i
-if "%PROCESSOR_ARCHITECTURE%"=="x86" (
-start /b "" "%CD%"/AutoIt3.exe "%a3x%"
-) else (
-start /b "" "%CD%"/AutoIt3_x64.exe "%a3x%"
-)
